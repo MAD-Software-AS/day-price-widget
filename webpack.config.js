f365bc833ed093f197fffdd74bd98c9cf9a9ccbb
VladBogach <bogach.vladislav@gmail.com>
2025-11-10 12:24:42 +0200

    update widget name

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,52 @@
 const path = require('path')
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production'
 
 // get the current package.json version
-const { version } = require('./package.json');
-const semver = require('semver');
+const { version } = require('./package.json')
+const semver = require('semver')
 
 // extract the major (left-most) number
-const { major } = semver.parse(version);
+const { major } = semver.parse(version)
 
 module.exports = {
   entry: './src/index.tsx',
   mode: isProduction ? 'production' : 'development',
   output: {
-    library: 'MAD',
+    library: 'MAD_DAY_PRICE_WIDGET',
     libraryTarget: 'umd',
     libraryExport: 'default',
     path: path.resolve(__dirname, 'dist', `v${major}`),
-    filename: `widget${isProduction ? '.min' : ''}.js`,
+    filename: `widget${isProduction ? '.min' : ''}.js`
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js']
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: [
-          { loader: 'ts-loader' },
-        ],
+        use: [{ loader: 'ts-loader' }]
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel-loader'
       },
       {
         test: /\.css$/,
-        use: ['css-loader', 'postcss-loader'],
-      },
-    ],
+        use: ['css-loader', 'postcss-loader']
+      }
+    ]
   },
   plugins: [
     isProduction
       ? undefined
       : new HTMLWebpackPlugin({
-        template: path.resolve(__dirname, 'index.html'),
-      }),
-    isProduction ? undefined : new webpack.HotModuleReplacementPlugin(),
-  ].filter(Boolean),
-};
+          template: path.resolve(__dirname, 'index.html')
+        }),
+    isProduction ? undefined : new webpack.HotModuleReplacementPlugin()
+  ].filter(Boolean)
+}
